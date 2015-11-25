bbbd2babc55e6048a9a9fd2c888c03248e4a24ed
ToshioMutsuda <muttune@outlook.jp>
2015-11-25 10:41:49 +0900

    コメント修正
    コメント修正

--- 6fw-project/6fw-ver0.02/index.php
+++ 6fw-project/6fw-ver0.02/index.php
@@ -1,10 +1,10 @@
-<?php
+ï»¿<?php
 /**
  * Main Module and Routing contoroller of framework
  *
  */
  
 header("location: Controller/Controller{$_GET['param1']}.php?param1={$_GET['param1']}");
-exit();//©–Y‚ê‚¸‚ÉI
+exit();//â†å¿˜ã‚Œãšã«ï¼
 
 ?>
